c7b38e11b4c351a3c6c0962fddd383b013c4b26f
cocomon173 <66151128+cocomon173@users.noreply.github.com>
2020-09-08 22:01:51 +0900

    Update encryp.py

--- encryp.py
+++ encryp.py
@@ -3,8 +3,8 @@ import os
 key = Fernet.generate_key() 
 with open("key.key",'wb') as f:
     f.write(key)
-input_file = input("암호화 할 파일명(확장자 포함): ")
-output_file = input("암호화파일명(확장자 제외): ")+'.encrypted'
+input_file = input("File You going to encrypt(With File extension): ")
+output_file = input("OutPut file name(Without File extension): ")+'.encrypted'
 
 with open(input_file, 'rb') as f:
     data = f.read()  
